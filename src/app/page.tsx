619eb32f12f836354888e5cc7a6ce02ad97abdf2
Thirawat74 <sawn4568@gmail.com>
2026-01-09 16:43:39 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { HeroSection } from '@/components/sections/hero';
-
+import { HeroSection } from "@/components/sections/hero";
+import { QuoteImageSection } from "@/components/sections/quote-image";
 
 export default function Home() {
   return (
     <main className="w-full">
-        <HeroSection />
-
+      <HeroSection />
+      <QuoteImageSection />
     </main>
   );
 }
