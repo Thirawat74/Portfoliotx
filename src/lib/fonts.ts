fdbb27ac94acc76e57b94beb1030676e7ea1bebe
Thirawat74 <sawn4568@gmail.com>
2026-01-09 18:04:55 +0700

    Delete src/lib/fonts.ts

--- src/lib/fonts.ts
+++
@@ -1,7 +0,0 @@
-import { Line_Seed_Sans_TH } from "next/font/google";
-
-export const lineSeed = Line_Seed_Sans_TH({
-  subsets: ["thai", "latin"],
-  weight: ["400", "700"],
-  display: "swap",
-});
